875ec0d9bcc4cb7631f870a217d7515af4b76937
kaperkin <kaleena.perkins@gmail.com>
2015-08-19 10:46:34 -0700

    form validation, reset form function added

--- js/contact.js
+++ js/contact.js
@@ -3,13 +3,20 @@
 /////////////////////////////////////////////////////////
 function init() {
 	// variable declarations
-	var radioBtns = document.getElementsByClassName("radioBtns");
-	window.otherBtn = document.getElementById("otherBtn");
-	window.otherExplain = document.getElementById("otherExplain");
+    window.firstName = document.getElementById("firstName");
+    window.lastName = document.getElementById("lastName");
+    window.emailAddress = document.getElementById("email");
     window.photo = document.getElementById("photo");
     window.link = document.getElementById("link");
-    window.photocheckbox = document.getElementById("photoCheckbox");
+    window.radioBtns = document.getElementsByClassName("radioBtns");
+	window.otherBtn = document.getElementById("otherBtn");
+	window.otherExplain = document.getElementById("otherExplain");
+	window.caption = document.getElementById("caption");
+	window.geoLocation = document.getElementById("geoLocation");
+    window.photoCheckbox = document.getElementById("photoCheckbox");
     window.linkCheckbox = document.getElementById("linkCheckbox");
+    window.photo = document.getElementById("photo");
+    window.link = document.getElementById("link");
 
     // display styling
 	otherExplain.style.display = "none";
@@ -17,7 +24,9 @@ function init() {
 	link.style.display="none";
 
     // add Event Listeners
-    photocheckbox.addEventListener("change", showPhotoBox);
+    document.getElementById("cancelBtn").addEventListener("click", cancelForm);
+    document.getElementById("submitBtn").addEventListener("click", validateForm);
+    photoCheckbox.addEventListener("change", showPhotoBox);
     linkCheckbox.addEventListener("change", showLinkBox);
 	//adds event listener to each radio button
 	for (var i = 0; i < radioBtns.length; i++) {
@@ -25,6 +34,7 @@ function init() {
 		radioBtns[i].addEventListener("change", showOtherExplain);
 	}
 }
+
 //////// calls init function when DOM loads ////////////
 document.addEventListener("DOMContentLoaded", init);
 
@@ -39,7 +49,7 @@ function showOtherExplain() {
 
 //////// Show photo input when checkbox is checked ////////
 function showPhotoBox(){
-	if (photocheckbox.checked){
+	if (photoCheckbox.checked){
 		photo.style.display="block";
 	}else{
 		photo.style.display="none";
@@ -53,4 +63,42 @@ function showLinkBox(){
 	}else{
 		link.style.display="none";
 	}
+}
+
+//////// reset contact form////////
+function cancelForm(){
+    // resets form to original values unless changed with error messages //
+    document.getElementById("contact").reset();
+    // resets placeholder values //
+    firstName.setAttribute("placeholder", "First Name");
+    geoLocation.setAttribute("placeholder", "Location");
+    // hides optional inputs //
+    showPhotoBox();
+    showLinkBox();
+    showOtherExplain();
+//    // remove error class from required fields //
+    if (firstName.getAttribute("class", "error")){
+        firstName.classList.remove("error");
+    }
+    if(geoLocation.getAttribute("class", "error")){
+        geoLocation.classList.remove("error");
+    }
+}
+
+//////// validate fields on form when submit button pressed ////////
+function validateForm(){
+    //validate first name at least 2 letters long
+    if(firstName.value.length < 2){
+        firstName.classList.add("error");
+        firstName.setAttribute("placeholder", "FIRST NAME IS REQUIRED");
+    }else{
+        firstName.classList.remove("error");
+    }
+     //validate geoLocation at least 10 letters long
+    if(geoLocation.value.length < 8){
+        geoLocation.classList.add("error");
+        geoLocation.setAttribute("placeholder", "LOCATION IS REQUIRED");
+    }else{
+        geoLocation.classList.remove("error");
+    }
 }
