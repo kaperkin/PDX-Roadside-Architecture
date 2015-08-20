53407f2d737290c8cf5f9fc795c418de77a3451b
kaperkin <kaleena.perkins@gmail.com>
2015-08-20 16:51:43 -0700

    added Django framework with functional contact page

---
+++ pdx_road/pdx_road_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Submission(models.Model):
+    firstName = models.CharField(max_length = 200)
+    lastName = models.CharField(max_length = 200)
+    emailAddress = models.CharField(max_length = 200)
+    category = models.CharField(max_length = 200)
+    otherCategoryExplain = models.TextField()
+    caption = models.CharField(max_length = 200)
+    geoLocation = models.CharField(max_length = 200)
+    photo = models.CharField(max_length=200)#how to do a photo upload
+    link = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.geoLocation
---
+++ pdx_road/pdx_road_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from models import Submission
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+@csrf_exempt
+def make_submission(request):
+    if request.POST:
+        submission = Submission()
+        submission.firstName = request.POST["firstName"]
+        submission.lastName = request.POST["lastName"]
+        submission.emailAddress = request.POST["emailAddress"]
+        submission.category = request.POST["category"]
+        submission.otherCategoryExplain = request.POST["otherExplain"]
+        submission.caption = request.POST["caption"]
+        submission.geoLocation = request.POST["geoLocation"]
+        submission.photo = request.POST["photo"]
+        submission.link = request.POST["link"]
+        submission.save()
+        return HttpResponse(str(submission.id))
+
+
